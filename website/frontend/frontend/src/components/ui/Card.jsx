f3c56c7c5fa8199bb2d1ec74b02d777904300cca
Yavar <zuri00014@gmail.com>
2025-11-05 16:48:51 +0530

    major website work completed working with mock data as of now

---
+++ website/frontend/frontend/src/components/ui/Card.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { cn } from "../../lib/utils";
+
+export function Card({ className, ...props }) {
+  return (
+    <div
+      className={cn(
+        "rounded-xl border  border-gray-200shadow-sm",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export function CardContent({ className, ...props }) {
+  return <div className={cn("p-4", className)} {...props} />;
+}
