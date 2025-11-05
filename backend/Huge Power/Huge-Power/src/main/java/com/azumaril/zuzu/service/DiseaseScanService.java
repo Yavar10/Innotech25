6adab0b9f06b47ee1316c829617a135dbea9895f
Mradul <mkjmp77@gmail.com>
2025-11-05 10:32:37 +0530

    Initial Commit

---
+++ backend/Huge Power/Huge-Power/src/main/java/com/azumaril/zuzu/service/DiseaseScanService.java
@@ -0,0 +1,22 @@
+package com.azumaril.zuzu.service;
+
+import com.azumaril.zuzu.entity.DiseaseScan;
+import com.azumaril.zuzu.repository.DiseaseScanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class DiseaseScanService {
+
+}
